1b8446e58ef87cc22fca90ae29f1ce3bd1b33a2d
664962185 <664962185@qq.com>
2014-09-24 00:36:20 +0800

    Revert "gaoruan"
    This reverts commit 7d8fe58a7ffcfa1c83a4fa3b2e5dad8e069026dd.

--- gaoruan1.c
+++ gaoruan1.c
@@ -31,75 +31,77 @@ int Help();
 
 typedef struct DataNode
 {
-    char* cmd;
-    char* desc;
-    int   (*handler)();
-    struct DataNode *next;
+	char* cmd;
+	char* desc;
+	int   (*handler)();
+	struct DataNode *next;
 } tDataNode;
 
 tDataNode* FindCmd(tDataNode *head,char *cmd)
 {
-    if(head==NULL||cmd==NULL)
-    {
-        return NULL;
-    }
-    tDataNode *p=head;
-    while(p!=NULL)
-    {
-        if(!strcmp(p->cmd,cmd))
-        {
-             return p;
+	if(head==NULL||cmd==NULL)
+	{
+		return NULL;
 	}
-	p=p->next;
-    }
-    return NULL;
+	tDataNode *p=head;
+	while(p!=NULL)
+	{
+		if(!strcmp(p->cmd,cmd))
+		{
+			return p;
+		}
+		p=p->next;
+	}
+	return NULL;
+
 }
 
 int ShowAllCmd(tDataNode *head)
 {
-    printf("Menu List:\n");
-    tDataNode *p=head;
-    while(p!=NULL)
-    {
-        printf("%s - %s\n",p->cmd,p->desc);
-        p=p->next;
-    }
-    return 0;
+	printf("Menu List:\n");
+	tDataNode *p=head;
+	while(p!=NULL)
+	{
+		printf("%s - %s\n",p->cmd,p->desc);
+		p=p->next;
+	}
+	return 0;
+
 }
 
 /* menu program */
 
 static tDataNode head[]=
 {
-    {"help","this is help cmd!",Help,&head[1]},
-    {"version","menu program v1.0",NULL,NULL}
+	{"help","this is help cmd!",Help,&head[1]},
+	{"version","menu program v1.0",NULL,NULL}
 };
 
 main()
 {
 	
-    /* cmd line begins */
-    while(1)
-    {
-        char cmd[CMD_MAX_LEN];
-	printf("Input a cmd number >");
-	scanf("%s",cmd);
-	tDataNode *p=FindCmd(head,cmd);
-	if(p==NULL)
-	{	
-	    printf("This is a wrong cmd!\n");
-	    continue;
-	}
-	printf("%s - %s\n",p->cmd,p->desc);
-	if(p->handler!=NULL)
+	/* cmd line begins */
+	while(1)
 	{
-	    p->handler();
-	}	
-    }
+		char cmd[CMD_MAX_LEN];
+		printf("Input a cmd number >");
+		scanf("%s",cmd);
+		tDataNode *p=FindCmd(head,cmd);
+		if(p==NULL)
+		{	
+			printf("This is a wrong cmd!\n");
+			continue;
+		}
+		printf("%s - %s\n",p->cmd,p->desc);
+		if(p->handler!=NULL)
+		{
+			p->handler();
+		}	
+	}
 }
 
 int Help()
 {
-    ShowAllCmd(head);
-    return 0;
+	ShowAllCmd(head);
+	return 0;
 }
